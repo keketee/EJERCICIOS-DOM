d17d96d87a8d0b30f4c0386e034a9405d6b8d80c
Diegohhallam <elgrankeko@hotmail.com>
2024-06-21 18:59:00 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+
+const starPointElemente = document.getElementById('start-point')
+starPointElemente.previousElementSibling.textContent = 'cambio titulo'
+starPointElemente.textContent= 'hola'
+
+
+
+console.log(`Soy un ${starPointElemente.tagName} con el id ${starPointElemente.id} y la clase ${starPointElemente.className} mi padre es un ${starPointElemente.parentElement.tagName} con el id ${starPointElemente.parentElement.id} y la clase es ${starPointElemente.parentElement.className}, mi hermano es un ${starPointElemente.previousElementSibling.previousElementSibling.tagName} con el id ${starPointElemente.previousElementSibling.previousElementSibling.id} con la clase ${starPointElemente.previousElementSibling.previousElementSibling.className} y mi otro hermano es un ${starPointElemente.previousElementSibling.tagName} con un id ${starPointElemente.previousElementSibling.id} y con la clase ${starPointElemente.previousElementSibling.className}  y su contenido seria ${starPointElemente.previousElementSibling.textContent}`)
